c01cd9825d975f2324ea241902ca32ce77d45c97
omkumar0417 <omkumar201206@gmail.com>
2025-07-10 18:33:13 +0530

    Your update message

--- notes.js
+++ notes.js
@@ -3,17 +3,18 @@ const notesData = {
     {
       "title": "Introduction to Java",
       "date": "2025-07-08",
-      "content": "Java is a high-level programming language...",
-      "image": ""
+      "content": "Java is a high-level programming language... 88",
+      "image": "https://i.postimg.cc/3RhrPJVG/temp-Image-KGb-Kf-L.avif"
     }
   ],
-  "DSA": [
+  "DSA": [],
+  "dda": [],
+  "Servlet": [
     {
-      "title": "Introduction to Arrays",
+      "title": "Introduction to Servlets",
+      "content": "A Servlet is a Java class used to handle HTTP requests and generate dynamic web content.\nIt runs on a server and extends the capabilities of a web server.\nServlets are platform-independent, secure, efficient, and portable.\nLifecycle of a servlet includes:\nInitialization (init method)\nRequest Handling (service method)\nDestruction (destroy method)\nIt uses javax.servlet and javax.servlet.http packages.",
       "date": "2025-07-08",
-      "content": "Arrays are used to store multiple values...",
       "image": ""
     }
-  ],
-  "hello": []
+  ]
 };
