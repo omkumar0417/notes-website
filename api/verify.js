8f4f364faa4082c90d3320edb9005a6740acf281
omkumar0417 <omkumar201206@gmail.com>
2025-07-27 15:45:12 +0530

    Your update message

--- api/verify.js
+++ api/verify.js
@@ -1,8 +1,21 @@
+// export default function handler(req, res) {
+//   const token = req.headers.authorization?.split(" ")[1];
+//   if (token === process.env.ACCESS_TOKEN) {
+//     res.status(200).json({ loggedIn: true });
+//   } else {
+//     res.status(200).json({ loggedIn: false });
+//   }
+// }
 export default function handler(req, res) {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (token === process.env.ACCESS_TOKEN) {
-    res.status(200).json({ loggedIn: true });
+  const validToken = "om123access4567"; // ✅ PLACE HERE
+
+  const authHeader = req.headers.authorization || "";
+  const token = authHeader.split(" ")[1]; // "Bearer om123access4567" => "om123access4567"
+
+  if (token === validToken) {
+    res.status(200).json({ verified: true });
   } else {
-    res.status(200).json({ loggedIn: false });
+    res.status(401).json({ verified: false });
   }
 }
+
